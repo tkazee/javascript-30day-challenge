c836a46408444258e7935213c043d07b3af8dec7
Azeem <Azeemtk9@gmail.com>
2024-07-23 18:11:06 +0530

    Initial

---
+++ 1.var & datatypes/1.var.js
@@ -0,0 +1,14 @@
+//Task 1 
+/*
+Declare a variable using var
+
+ */
+var numberVar=21;
+console.log(numberVar);
+
+//Task 2
+/*
+Declare a variable using Let
+ */
+let stringVar="hello";
+console.log(stringVar);
---
+++ 1.var & datatypes/3.Activity.js
@@ -0,0 +1,22 @@
+//Data Types
+let numVar= 20;
+console.log(typeof numVar) //number
+
+//string
+let strVar="Hello";
+console.log(strVar);
+
+//Boolean 
+let boolVar=true;
+console.log(boolVar);
+//object
+let objVar={
+    name:'Azeem',
+    age:20
+}
+console.log(typeof objVar);
+
+//Array
+let arrayVar= [1,2,3,4,5];
+console.log(typeof arrayVar); //object (Arrays are a type of object in js)
+
