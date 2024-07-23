8bf39271553dd19a3e1e99458a866535d102e228
Azeem <Azeemtk9@gmail.com>
2024-07-23 18:42:49 +0530

    added

---
+++ 1.var & datatypes/4.reassinging-values.js
@@ -0,0 +1,5 @@
+let variable = 'initail value';
+console.log(variable)
+
+variable = 'New Value';
+console.log(variable);
---
+++ 1.var & datatypes/5.Understand const.js
@@ -0,0 +1,10 @@
+const constantVar=10;
+console.log(constantVar);
+
+//Attempting to reassign
+constantVar=20;
+console.log(constantVar);
+/* when you run the code above you will
+see an error in the console
+TypeError: Assignment to constant varible
+*/
